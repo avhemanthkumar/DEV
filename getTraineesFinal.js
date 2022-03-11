6535b0a6386f9069766e451a5dc58fc1cc9daec5
avhemanthkumar <101056506+avhemanthkumar@users.noreply.github.com>
2022-03-11 06:24:59 -0500

    Update getTraineesFinal.js

--- getTraineesFinal.js
+++ getTraineesFinal.js
@@ -13,6 +13,8 @@ export default class GetTrainees extends LightningElement{
 //@track SelectedtraineesList = [];
 @api recordId;
 searchKey = '';
+data = [];
+selection = [];
 
  
 
@@ -27,6 +29,47 @@ retrieveTrainees({error,data}){
 
     }
 }
+rowSelection(evt) {
+    // List of selected items from the data table event.
+    let updatedItemsSet = new Set();
+    // List of selected items we maintain.
+    let selectedItemsSet = new Set(this.selection);
+    // List of items currently loaded for the current view.
+    let loadedItemsSet = new Set();
+
+
+    this.data.map((event) => {
+        loadedItemsSet.add(event.Id);
+    });
+
+
+    if (evt.detail.selectedRows) {
+        evt.detail.selectedRows.map((event) => {
+            updatedItemsSet.add(event.Id);
+        });
+
+
+        // Add any new items to the selection list
+        updatedItemsSet.forEach((id) => {
+            if (!selectedItemsSet.has(id)) {
+                selectedItemsSet.add(id);
+            }
+        });        
+    }
+
+
+    loadedItemsSet.forEach((id) => {
+        if (selectedItemsSet.has(id) && !updatedItemsSet.has(id)) {
+            // Remove any items that were unselected.
+            selectedItemsSet.delete(id);
+        }
+    });
+
+
+    this.selection = [...selectedItemsSet];
+    console.log('---selection---'+JSON.stringify(this.selection));
+    
+  }
 
 handleKeyChange(event) {
 
@@ -38,15 +81,20 @@ addRecord(){
     var selectedRecords =  
      this.template.querySelector("lightning-datatable").getSelectedRows();  
      createTraineelineItems({selectedRecords: selectedRecords, recordId:this.recordId})  
-    .then(result=>{  
+     .then(result=>{  
         const evt = new ShowToastEvent({
             title:'Success Message',
-            message:'Trainess Added successfully! Close the pop-up and refresh the Page.',
+            message:'Trainess Added successfully! Refresh the Page.',
             variant:'Success',
             mode:'dismissable'
         });
         this.dispatchEvent(evt);
-      return refreshApex(this.traineesList);  
+        setTimeout(
+            function() {
+                window.history.back();
+            },
+            10
+        ); 
     })  
     .catch(error=>{  
       alert('Could not Add'+JSON.stringify(error));  
