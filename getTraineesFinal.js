3a6c84cdb4a278d4ee14312793b487a036524d04
avhemanthkumar <101056506+avhemanthkumar@users.noreply.github.com>
2022-03-15 05:59:33 -0400

    Update getTraineesFinal.js

--- getTraineesFinal.js
+++ getTraineesFinal.js
@@ -3,22 +3,31 @@ import {refreshApex} from '@salesforce/apex';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import createTraineelineItems from '@salesforce/apex/getRecordDataController.createTraineelineItems';
 import getTraineesList from '@salesforce/apex/getRecordDataController.getTraineesList';
+import getTraineesData from '@salesforce/apex/getRecordDataController.getTraineesData';
 import { CloseActionScreenEvent } from 'lightning/actions';
 const columns = [
     { label: 'Name', fieldName: 'Name',type:'text' }];
+const columns2= [
+    { label: 'Name', fieldName: 'Name',type:'text' },
+    { label: 'Id', fieldName: 'Id',type:'text' }
+]
 
 export default class GetTrainees extends LightningElement{
 
-@track traineesList = [];
-//@track SelectedtraineesList = [];
+traineesList = [];
+@track SelectedtraineesId = [];
+@track SelectedtraineesName=[];
+ SelectedtraineesList = [];
 @api recordId;
 searchKey = '';
 data = [];
-selection = [];
+showText = false;
+searchData;
 
  
 
 Columns = columns;
+Columns2 = columns2;
 
 @wire(getTraineesList,{searchKey:'$searchKey'})
 retrieveTrainees({error,data}){
@@ -29,76 +38,71 @@ retrieveTrainees({error,data}){
 
     }
 }
-rowSelection(evt) {
-    // List of selected items from the data table event.
-    let updatedItemsSet = new Set();
-    // List of selected items we maintain.
-    let selectedItemsSet = new Set(this.selection);
-    // List of items currently loaded for the current view.
-    let loadedItemsSet = new Set();
+handleClick(event){
+    this.showText = true;
+    getTraineesData({Ids : this.SelectedtraineesId})
+        .then(result => {
+            this.searchData = result;
+            
+        })
+        .catch(error => {
+            this.searchData = undefined;
+            window.console.log('error =====> '+JSON.stringify(error));
+            
+        }) 
 
-
-    this.data.map((event) => {
-        loadedItemsSet.add(event.Id);
-    });
-
-
-    if (evt.detail.selectedRows) {
-        evt.detail.selectedRows.map((event) => {
-            updatedItemsSet.add(event.Id);
-        });
-
-
-        // Add any new items to the selection list
-        updatedItemsSet.forEach((id) => {
-            if (!selectedItemsSet.has(id)) {
-                selectedItemsSet.add(id);
-            }
-        });        
-    }
-
-
-    loadedItemsSet.forEach((id) => {
-        if (selectedItemsSet.has(id) && !updatedItemsSet.has(id)) {
-            // Remove any items that were unselected.
-            selectedItemsSet.delete(id);
-        }
-    });
-
-
-    this.selection = [...selectedItemsSet];
-    console.log('---selection---'+JSON.stringify(this.selection));
     
-  }
+}
 
 handleKeyChange(event) {
 
-    this.searchKey = event.target.value;
+    const searchString = event.target.value;
+    this.searchKey = searchString;
      
     
 }
-addRecord(){  
-    var selectedRecords =  
-     this.template.querySelector("lightning-datatable").getSelectedRows();  
-     createTraineelineItems({selectedRecords: selectedRecords, recordId:this.recordId})  
-     .then(result=>{  
-        const evt = new ShowToastEvent({
-            title:'Success Message',
-            message:'Trainess Added successfully! Refresh the Page.',
-            variant:'Success',
-            mode:'dismissable'
-        });
-        this.dispatchEvent(evt);
-        setTimeout(
-            function() {
-                window.history.back();
-            },
-            10
-        ); 
-    })  
-    .catch(error=>{  
-      alert('Could not Add'+JSON.stringify(error));  
-    })  
+
+handleAccountClick(event) {
+    const selectedAccId = event.detail.selectedRows;
+    window.console.log('selectedAccId# ' + JSON.stringify(selectedAccId));
+    this.selectedAccId=[];
+    
+    
+    for (let i = 0; i<selectedAccId.length; i++){ 
+        this.SelectedtraineesId.push(selectedAccId[i].Id);
+        this.SelectedtraineesName.push(selectedAccId[i].Name);
+        /*var Selectedtrainees = new Object ();
+        Selectedtrainees.Name = this.SelectedtraineesName;
+        Selectedtrainees.Id = this.SelectedtraineesId;
+        this.SelectedtraineesList.push(Selectedtrainees);
+        window.console.log(Selectedtrainees);*/
+    }
+
+    
+    window.console.log(this.SelectedtraineesList);
+
+}
+addRecord(){    
+    createTraineelineItems({selectedRecords: this.SelectedtraineesId, recordId:this.recordId})  
+    .then(()=>{ 
+        this.template.querySelector('lightning-datatable').selectedAccId=[]; 
+       const evt = new ShowToastEvent({
+           title:'Success Message',
+           message:'Trainess Added successfully! Refresh the Page.',
+           variant:'Success',
+           mode:'dismissable'
+       });
+       this.dispatchEvent(evt);
+       setTimeout(
+           function() {
+               window.history.back();
+           },
+           10
+       ); 
+   })  
+   .catch(error=>{  
+     alert('Could not Add'+JSON.stringify(error));  
+   }) 
   }
   closeQuickAction() {
     this.dispatchEvent(new CloseActionScreenEvent());
